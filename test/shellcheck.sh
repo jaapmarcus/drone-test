69699d8a34b71d38aad229a6cc86bcf473e300ca
Jaap Marcus <9754650+jaapmarcus@users.noreply.github.com>
2021-10-19 11:42:41 +0000

    fwefawe

--- test/shellcheck.sh
+++ test/shellcheck.sh
@@ -32,9 +32,9 @@ for file in $files; do
     echo "Linting: $file"
     shellcheck -x "$file" --severity="error" -e "SC2086,SC2002,SC2153,SC2181,SC2153,SC2129,SC2016,SC2196,SC1090,SC2031,SC2010,SC2143,SC2046" 
     if [ $? -gt 0 ]; then 
-        echo "\e[31mFAIL\e[39m"; err=1;
+        echo "\033[0;31m FAIL"; err=1;
     else 
-        echo "\e[32mOK\e[39m"
+        echo "\032[0;31m OK"
         shellcheck -x "$file" --severity="info" -e "SC2086,SC2002,SC2153,SC2181,SC2153,SC2129,SC2016,SC2196,SC1090,SC2031,SC2010,SC2143,SC2046" 
     fi
 done
